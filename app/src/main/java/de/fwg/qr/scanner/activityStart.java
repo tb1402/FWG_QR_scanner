4a0f5bf81fad5f753abb2acf160ef34af9596abc
Benni013 <welschbenkc@t-online.de>
2020-06-19 23:39:39 +0200

    fixed ui from previous commit (3bab4053d609e2b763768442b0152e655c305fa8) extracted white and black color resources

--- app/src/main/java/de/fwg/qr/scanner/activityStart.java
+++ app/src/main/java/de/fwg/qr/scanner/activityStart.java
@@ -1,12 +1,7 @@
 package de.fwg.qr.scanner;
 
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
@@ -32,7 +27,8 @@ public class activityStart extends toolbarWrapper {
 
     @Override
     public void onBackPressed() {
-        LayoutInflater inflater = getLayoutInflater();
+        // dark mode Toast
+        /*LayoutInflater inflater = getLayoutInflater();
         View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.custom_toast_container));
 
         TextView text = (TextView) layout.findViewById(R.id.text);
@@ -42,7 +38,8 @@ public class activityStart extends toolbarWrapper {
         toast.setGravity(Gravity.BOTTOM, 0, 96);
         toast.setDuration(Toast.LENGTH_SHORT);
         toast.setView(layout);
-        toast.show();
-        //Toast.makeText(getApplicationContext(), "Bitte beenden Sie erst die StartGuide", Toast.LENGTH_SHORT).show();
+        toast.show();*/
+
+        Toast.makeText(getApplicationContext(), "Bitte beenden Sie erst die StartGuide", Toast.LENGTH_SHORT).show();
     }
 }
