8d41744594785221ccf23d29511c6cc2f7c84d93
Deko29 <max.bal@gmx.net>
2020-07-20 12:07:36 +0200

    -Changes:    Added things

--- app/src/main/java/de/fwg/qr/scanner/map/mapBuilding.java
+++ app/src/main/java/de/fwg/qr/scanner/map/mapBuilding.java
@@ -1,11 +1,17 @@
 package de.fwg.qr.scanner.map;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
+import de.fwg.qr.scanner.activityErrorHandling;
 import de.fwg.qr.scanner.tools.network;
 import de.fwg.qr.scanner.tools.networkCallbackInterface;
 
@@ -16,11 +22,15 @@ public class mapBuilding implements networkCallbackInterface {
     private ArrayList<Bitmap> stockwerk;
 
     private Bitmap bitmap;
+    private Canvas canvas;
 
     private network net;
     private WeakReference<networkCallbackInterface> ref;
 
     private int i = 0;
+    private int length = -1;
+
+    private Intent intent;
 
     public mapBuilding(Context context, int level) {
         this.context = context;
@@ -29,29 +39,62 @@ public class mapBuilding implements networkCallbackInterface {
         String floor = "";
         switch (level) {
             case 0:
-                floor = "Erdgeschoss";
+                floor = "erdgeschoss";
                 break;
             case 1:
-                floor = "1. Stockwerk";
+                floor = "stock1";
                 break;
             case 2:
-                floor = "2.Stockwerk";
+                floor = "stock2";
                 break;
+
         }
         net.makeImageRequest(ref, "ImageRequest", floor, i, true);
     }
 
     @Override
     public void onPostCallback(String operation, String response) {
+        //lockUI(false);
+        if (operation.contains("error") || response.contains("Error") || response.contains("error")) {
+            intent = new Intent();
+            Intent i = new Intent(context, activityErrorHandling.class);
+            i.putExtra(activityErrorHandling.errorNameIntentExtra, response);
+            context.startActivity(intent);
+        }
+        if (operation.contentEquals("getInfo")) {
+            try {
+                JSONObject object = new JSONObject(response);
+                length = Integer.parseInt(object.getString("Bild"));
 
+            } catch (JSONException e) {
+                intent = new Intent();
+                Intent i = new Intent(context, activityErrorHandling.class);
+                i.putExtra(activityErrorHandling.errorNameIntentExtra, activityErrorHandling.stackTraceToString(e));
+                context.startActivity(intent);
+            }        //lockUI(false);
+            if (operation.contains("error") || response.contains("Error") || response.contains("error")) {
+                intent = new Intent();
+                Intent i = new Intent(context, activityErrorHandling.class);
+                i.putExtra(activityErrorHandling.errorNameIntentExtra, response);
+                context.startActivity(intent);
+            }
+
+        }
+
+        @Override
+        public void onImageCallback (String name, Bitmap image){
+            if (name.contentEquals("ImageRequest")) {
+
+
+            }
+
+        }
+
+        public Bitmap getBitmap () {
+            return bitmap;
+        }
+
+        public void getImages () {
+            for (int x = 0, x)
+        }
     }
-
-    @Override
-    public void onImageCallback(String name, Bitmap image) {
-
-    }
-
-    public Bitmap getBitmap() {
-        return bitmap;
-    }
-}
